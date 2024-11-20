aea1a40dacf2649500c52fdbed17d44da2fe5e62
Stefano <sleto1999@icloud.com>
2024-11-20 12:16:47 +0100

    [Add] new battery colours

--- components/Layouts/LayoutPostRegistration.tsx
+++ components/Layouts/LayoutPostRegistration.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Battery from "../Battery/Battery";
+import BatterySemiFull from "../Battery/BatterySemiFull";
+import BatteryHalfFull from "../Battery/BatteryHalfFull";
+import BatterySemiEmpty from "../Battery/BatterySemiEmpty";
+import BatteryEmpty from "../Battery/BatteryEmpty";
 import Image from "next/image";
 import aggregatorImg from "../../public/aggregator.png";
 import batteriesData from "../../db/batteries.json";
@@ -14,49 +18,63 @@ const LayoutPostRegistration: React.FC<LayoutPostRegistrationProps> = ({
   handleOpenDialog,
   visibleBatteriesCount,
 }) => {
+  const getBatteryComponent = (SoC: number) => {
+    if (SoC >= 80) return Battery;
+    if (SoC >= 60) return BatterySemiFull;
+    if (SoC >= 40) return BatteryHalfFull;
+    if (SoC >= 20) return BatterySemiEmpty;
+    return BatteryEmpty;
+  };
+
   return (
     <div className="flex-grow flex items-center justify-center w-full">
       <div className="grid grid-cols-2 gap-4 mr-8">
         {batteriesData
           .slice(0, Math.min(visibleBatteriesCount, 5))
-          .map((battery, idx) => (
-            <div key={idx} className="flex flex-col items-center">
-              <Battery key={idx} onClick={() => handleOpenDialog(idx)} />
-              <Typography
-                variant="caption"
-                className="mt-1 text-white font-semibold"
-                style={{
-                  color: "#83f765",
-                  backgroundColor: "#428f2f",
-                  padding: "2px 6px",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                }}
-              >
-                Battery {idx + 1}
-              </Typography>
-            </div>
-          ))}
+          .map((battery, idx) => {
+            const BatteryComponent = getBatteryComponent(battery.SoC);
+            return (
+              <div key={idx} className="flex flex-col items-center">
+                <BatteryComponent onClick={() => handleOpenDialog(idx)} />
+                <Typography
+                  variant="caption"
+                  className="mt-1 text-white font-semibold"
+                  style={{
+                    color: "#83f765",
+                    backgroundColor: "#428f2f",
+                    padding: "2px 6px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                  }}
+                >
+                  Battery {idx + 1}
+                </Typography>
+              </div>
+            );
+          })}
         {batteriesData
           .slice(5, Math.min(visibleBatteriesCount, 10))
-          .map((battery, idx) => (
-            <div key={idx} className="flex flex-col items-center">
-              <Battery key={idx} onClick={() => handleOpenDialog(idx + 5)} />
-              <Typography
-                variant="caption"
-                className="mt-1 text-white font-semibold"
-                style={{
-                  color: "#83f765",
-                  backgroundColor: "#428f2f",
-                  padding: "2px 6px",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                }}
-              >
-                Battery {idx + 6}
-              </Typography>
-            </div>
-          ))}
+          .map((battery, idx) => {
+            const BatteryComponent = getBatteryComponent(battery.SoC);
+            return (
+              <div key={idx} className="flex flex-col items-center">
+                <BatteryComponent onClick={() => handleOpenDialog(idx)} />
+                <Typography
+                  variant="caption"
+                  className="mt-1 text-white font-semibold"
+                  style={{
+                    color: "#83f765",
+                    backgroundColor: "#428f2f",
+                    padding: "2px 6px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                  }}
+                >
+                  Battery {idx + 6}
+                </Typography>
+              </div>
+            );
+          })}
       </div>
       <div className="flex flex-col items-center justify-center mx-8">
         <h2 className="font-bold text-lg mb-4">AGGREGATOR</h2>
@@ -69,44 +87,50 @@ const LayoutPostRegistration: React.FC<LayoutPostRegistrationProps> = ({
       <div className="grid grid-cols-2 gap-4 ml-8">
         {batteriesData
           .slice(10, Math.min(visibleBatteriesCount, 15))
-          .map((battery, idx) => (
-            <div key={idx} className="flex flex-col items-center">
-              <Battery key={idx} onClick={() => handleOpenDialog(idx + 10)} />
-              <Typography
-                variant="caption"
-                className="mt-1 text-white font-semibold"
-                style={{
-                  color: "#83f765",
-                  backgroundColor: "#428f2f",
-                  padding: "2px 6px",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                }}
-              >
-                Battery {idx + 11}
-              </Typography>
-            </div>
-          ))}
+          .map((battery, idx) => {
+            const BatteryComponent = getBatteryComponent(battery.SoC);
+            return (
+              <div key={idx} className="flex flex-col items-center">
+                <BatteryComponent onClick={() => handleOpenDialog(idx + 10)} />
+                <Typography
+                  variant="caption"
+                  className="mt-1 text-white font-semibold"
+                  style={{
+                    color: "#83f765",
+                    backgroundColor: "#428f2f",
+                    padding: "2px 6px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                  }}
+                >
+                  Battery {idx + 11}
+                </Typography>
+              </div>
+            );
+          })}
         {batteriesData
           .slice(15, Math.min(visibleBatteriesCount, 20))
-          .map((battery, idx) => (
-            <div key={idx} className="flex flex-col items-center">
-              <Battery key={idx} onClick={() => handleOpenDialog(idx + 15)} />
-              <Typography
-                variant="caption"
-                className="mt-1 text-white font-semibold"
-                style={{
-                  color: "#83f765",
-                  backgroundColor: "#428f2f",
-                  padding: "2px 6px",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                }}
-              >
-                Battery {idx + 16}
-              </Typography>
-            </div>
-          ))}
+          .map((battery, idx) => {
+            const BatteryComponent = getBatteryComponent(battery.SoC);
+            return (
+              <div key={idx} className="flex flex-col items-center">
+                <BatteryComponent onClick={() => handleOpenDialog(idx + 15)} />
+                <Typography
+                  variant="caption"
+                  className="mt-1 text-white font-semibold"
+                  style={{
+                    color: "#83f765",
+                    backgroundColor: "#428f2f",
+                    padding: "2px 6px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                  }}
+                >
+                  Battery {idx + 16}
+                </Typography>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
--- components/Layouts/LayoutPreRegistration.tsx
+++ components/Layouts/LayoutPreRegistration.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Battery from "../Battery/Battery";
+import BatterySemiFull from "../Battery/BatterySemiFull";
+import BatteryHalfFull from "../Battery/BatteryHalfFull";
+import BatterySemiEmpty from "../Battery/BatterySemiEmpty";
+import BatteryEmpty from "../Battery/BatteryEmpty";
 import Image from "next/image";
 import aggregatorImg from "../../public/aggregator.png";
 import energyGridImg from "../../public/smartgrid.png";
@@ -18,24 +22,40 @@ const LayoutPreRegistration: React.FC<LayoutPreRegistrationProps> = ({
       <div className="grid grid-cols-3 gap-8 w-full max-w-screen-lg">
         {/* Sezione Batterie (colonna sinistra) */}
         <div className="grid grid-cols-4 gap-4">
-          {batteriesData.slice(0, 20).map((battery, idx) => (
-            <div key={idx} className="flex flex-col items-center">
-              <Battery key={idx} onClick={() => handleOpenDialog(idx)} />
-              <Typography
-                variant="caption"
-                className="mt-1 text-white font-semibold"
-                style={{
-                  color: "#83f765",
-                  backgroundColor: "#428f2f",
-                  padding: "2px 2px",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                }}
-              >
-                Battery {idx + 1}
-              </Typography>
-            </div>
-          ))}
+          {batteriesData.slice(0, 20).map((battery, idx) => {
+            let BatteryComponent;
+
+            if (battery.SoC >= 80) {
+              BatteryComponent = Battery;
+            } else if (battery.SoC >= 60) {
+              BatteryComponent = BatterySemiFull;
+            } else if (battery.SoC >= 40) {
+              BatteryComponent = BatteryHalfFull;
+            } else if (battery.SoC >= 20) {
+              BatteryComponent = BatterySemiEmpty;
+            } else {
+              BatteryComponent = BatteryEmpty;
+            }
+
+            return (
+              <div key={idx} className="flex flex-col items-center">
+                <BatteryComponent onClick={() => handleOpenDialog(idx)} />
+                <Typography
+                  variant="caption"
+                  className="mt-1 text-white font-semibold"
+                  style={{
+                    color: "#83f765",
+                    backgroundColor: "#428f2f",
+                    padding: "2px 1px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                  }}
+                >
+                  Battery {idx + 1}
+                </Typography>
+              </div>
+            );
+          })}
         </div>
 
         {/* Sezione Aggregator (colonna centrale) */}
